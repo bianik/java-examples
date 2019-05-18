96ea50fc7e8fed8ae078a55da78408d23ebb0238
Niklas Fabian <niklas.fabian@rub.de>
2019-05-18 02:35:32 +0200

    new solution fully implemented and functional
    https://github.com/bianik/java-examples/issues/1

--- helloworld/src/helloworld/Helloworld.java
+++ helloworld/src/helloworld/Helloworld.java
@@ -3,8 +3,7 @@ package helloworld;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
+		final HelloMessage myMessage = new HelloMessage();
+		System.out.println(myMessage.getMessage());
 	}
-
 }
